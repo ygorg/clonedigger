57f1bcae7ca36724580adb1ade463c2a4f04df69
Ygor Gallina <ygor.gallina@gmail.com>
2020-04-17 15:14:38 +0200

    Reorganize `arguments.py`
    No modifications

--- clonedigger/arguments.py
+++ clonedigger/arguments.py
@@ -7,10 +7,10 @@ standard_library.install_aliases()
 from builtins import *
 
 clustering_threshold = 10
-hashing_depth = 1
 clusterize_using_dcup = False
-report_unifiers = False
-print_time = True
-force = False  
-use_diff = False 
 clusterize_using_hash = False
+hashing_depth = 1
+force = False
+use_diff = False
+print_time = True
+report_unifiers = False
